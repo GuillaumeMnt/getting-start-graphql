50aa252816490b1bf29c4efcf150ac5ae9e7133b
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-07-02 00:33:14 +0200

    feat: add multiple query with parameters

--- server.js
+++ server.js
@@ -2,10 +2,47 @@ const express = require("express");
 const express_graphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+// DB ... 🙊🙈
+const friends = [
+  {
+    id: 1,
+    name: "Guillaume",
+    gender: "M",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Lucas",
+    gender: "M",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "JM",
+    gender: "F",
+    age: 23,
+  },
+  {
+    id: 4,
+    name: "Yann",
+    gender: "nd",
+    age: 29,
+  },
+];
+
 // GraphQL Schema
 const schema = buildSchema(`
     type Query {
         helloYou(name: String!): String
+        giveGender(gender: String!): [User]
+        legalAge: [User]
+    }
+
+    type User {
+        id: Int
+        name: String
+        gender: String
+        age: Int
     }
 `);
 
@@ -13,8 +50,22 @@ const helloName = ({ name }) => {
   return `Hello ${name}!`;
 };
 
+const isLegalAge = ({}) => {
+  return friends.filter((user) => {
+    return user.age >= 18;
+  });
+};
+
+const giveGender = ({ gender }) => {
+  return friends.filter((user) => {
+    return user.gender === gender;
+  });
+};
+
 const root = {
   helloYou: helloName,
+  legalAge: isLegalAge,
+  giveGender: giveGender,
 };
 
 // Create an express server and GraphQL endpoint
