5f1a10e3780452932d44ece301a6dfb06f33defe
Guillaume Monot <guillaumemonotxy@gmail.com>
2020-07-01 23:25:04 +0200

    feat: add basic execution with graphql feat: launch graphiql

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const express_graphql = require("express-graphql");
+const { buildSchema, VariablesAreInputTypesRule } = require("graphql");
+
+// GraphQL Schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+const root = {
+  message: () => "Hello world!",
+};
+
+// Create an express server and GraphQL endpoint
+const app = express();
+
+app.use(
+  "/graphql",
+  express_graphql({
+    schema,
+    rootValue: root,
+    graphiql: VariablesAreInputTypesRule,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Express GraphQL server now running on localhost:4000/graphql");
+});
